eed35bb8c2496caaf7a5c8a1590be5d7002b0c97
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-09-30 12:16:06 +0200

    Update Spaces.cs

--- GJS/Spaces.cs
+++ GJS/Spaces.cs
@@ -11,18 +11,20 @@ namespace FinalSpaces
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("zadejte řetězec.");
+            string rt = Console.ReadLine();
+
+            Console.WriteLine("Na jakou délku chete prodloužit řetězec?");
+            int desired = int.Parse(Console.ReadLine());
 
 
-
-            var desired = 20;
-            string o = Console.ReadLine();
-            var rt = "AA   BB  CC";
+            //  var rt = "AA   BB  CC";
             //var rt = "F      JE   TU";
-             // var rt = "FJ      JE   TU   IN";
+            // var rt = "FJ      JE   TU   IN";
 
 
             List<List<int>> gross = new List<List<int>>();
-           // List<int> sub = new List<int>();
+            // List<int> sub = new List<int>();
             List<int> pos = new List<int>();
             List<int> spacecount = new List<int>();
 
@@ -51,8 +53,8 @@ namespace FinalSpaces
 
                 if (r == ' ')
                 {
-                   
-                    conv += 'X';
+
+                    conv += '#';
                 }
                 else
                 {
@@ -60,7 +62,7 @@ namespace FinalSpaces
                 }
             }
 
-            Console.WriteLine("CONV " + conv);
+            //   Console.WriteLine("CONV " + conv);
 
             // 46
 
@@ -71,49 +73,56 @@ namespace FinalSpaces
             {
                 //string c = String.valueOf(prestr[i]);
                 move.Add(conv[i].ToString());
-                
+
 
             }
 
             string e = "";
 
 
-            for (var i = 0; i < move.Count; i++){
-                Console.WriteLine("Element of move " + move[i]);
+            for (var i = 0; i < move.Count; i++)
+            {
+                // Console.WriteLine("Element of move " + move[i]);
             }
 
-            for (var i = 0; i < move.Count; i++){
+            for (var i = 0; i < move.Count; i++)
+            {
                 string s = move[i];
 
-                if (s == "X"){
-                    e += "X";
-                    Console.WriteLine("X is at " + i);
-                } else {
+                if (s == "#")
+                {
+                    e += "#";
+                    // Console.WriteLine("X is at " + i);
+                }
+                else
+                {
                     e += "-";
-                    Console.WriteLine("-");
-                    
+                    //  Console.WriteLine("-");
+
                 }
             }
 
-            Console.WriteLine("ELE");
-            Console.WriteLine(e);
+            //  Console.WriteLine("ELE");
+            //  Console.WriteLine(e);
 
 
 
             var fe = e.ToCharArray();
             for (var i = 0; i < fe.Length; i++)
             {
-                Console.WriteLine("Control " + fe[i]);
+                // Console.WriteLine("Control " + fe[i]);
             }
 
 
             List<int> fiedl = new List<int>();
 
-            for (var i = 0; i < fe.Length - 1; i++){
+            for (var i = 0; i < fe.Length - 1; i++)
+            {
                 string s = fe[i].ToString();
                 string inc = fe[i + 1].ToString();
 
-                if (s == "X" && inc == "-") {
+                if (s == "#" && inc == "-")
+                {
                     fiedl.Add(i + 1);
                 }
 
@@ -121,13 +130,18 @@ namespace FinalSpaces
 
 
             List<int> sub = new List<int>();
-            for (var i = 0; i < move.Count; i++){
+            for (var i = 0; i < move.Count; i++)
+            {
                 string w = move[i];
 
-                if (w == "X"){
+                if (w == "#")
+                {
                     sub.Add(i);
-                } else {
-                    if (sub.Count > 0) {
+                }
+                else
+                {
+                    if (sub.Count > 0)
+                    {
                         gross.Add(sub);
                         sub.Clear();
                     }
@@ -140,7 +154,8 @@ namespace FinalSpaces
 
             List<int> sf = new List<int>();
 
-            for (var i = 0; i < gross.Count; i++){
+            for (var i = 0; i < gross.Count; i++)
+            {
                 var el = gross[i];
                 sf.Add(Math.Abs(el.Count - desired / gross.Count));
                 ci += el.Count;
@@ -172,8 +187,8 @@ namespace FinalSpaces
 
 
 
-            Console.WriteLine("MC " + move.Count);
-        
+            //  Console.WriteLine("MC " + move.Count);
+
             for (var i = 0; i < move.Count; i++)
             {
                 var el = move[i];
@@ -181,37 +196,40 @@ namespace FinalSpaces
 
                 int mint = 0;
 
-                if (int.TryParse(el, out mint)){
-                  
-                    if (mint == 0){
+                if (int.TryParse(el, out mint))
+                {
+
+                    if (mint == 0)
+                    {
                         move.RemoveAt(i);
-                        move.Insert(i, "X");
+                        move.Insert(i, "#");
                     }
-               
+
 
                     for (var x = 0; x < mint; x++)
                     {
-                        res += "X";
-                        Console.WriteLine("HIHI " + i);
+                        res += "#";
+                        // Console.WriteLine("HIHI " + i);
                         move.RemoveAt(i);
                         move.Insert(i, res);
 
                     }
                 }
 
-               
-            
+
+
             }
 
 
 
             string save = "";
 
-            for (var i = 0; i < move.Count; i++){
+            for (var i = 0; i < move.Count; i++)
+            {
                 save += move[i];
             }
 
-            Console.WriteLine("7 " + save);
+            // Console.WriteLine("7 " + save);
             // 66
 
 
@@ -229,26 +247,33 @@ namespace FinalSpaces
             var pres = "";
 
 
-            for (var i = 0; i < chrr.Count; i++){
-                if (chrr[i] == "X"){
+            for (var i = 0; i < chrr.Count; i++)
+            {
+                if (chrr[i] == "#")
+                {
                     pres += " ";
 
-                } else {
+                }
+                else
+                {
                     pres += chrr[i];
                 }
             }
 
-            Console.WriteLine("P is " + pres);
+            // Console.WriteLine("P is " + pres);
 
 
             var arr = pres.ToCharArray();
 
             var er = "";
             for (var i = 0; i < arr.Length; i++)
-            { 
-                if (arr[i] == 'X'){
+            {
+                if (arr[i] == '#')
+                {
                     er += " ";
-                } else {
+                }
+                else
+                {
                     er += arr[i];
                 }
             }
