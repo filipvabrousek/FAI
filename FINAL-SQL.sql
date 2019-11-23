b8732e0e9334a4962c93f4f7b1cde58f9f629280
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-11-23 18:41:36 +0100

    Update FINAL-SQL.sql

--- FINAL-SQL.sql
+++ FINAL-SQL.sql
@@ -3,51 +3,80 @@ use CMS;
 
 
 CREATE TABLE Posts(
-idPost int not null primary key not null,
+idPost int not null primary key,
 published date not null,
 heading varchar(50) not null,
-content varchar(10000) not null,
+content varchar(10000) not null
 );
 
 CREATE TABLE Comments(
-idComment int not null primary key not null,
+idComment int not null primary key,
+idPost int not null,
 content varchar(50) not null,
-FOREIGN KEY idPost REFERENCES Post(idPost) ON DELETE CASCADE
+FOREIGN KEY (idPost) REFERENCES Posts(idPost) ON DELETE CASCADE
 );
 
-
+/*Key has to exist in both tables*/
 CREATE TABLE PostSettings(
-idSettings int primary key not nullm
+idSettings int primary key not null,
+idPost int not null,
 private int not null,
-FOREIGN KEY idPost REFERENCES Post(idPost) ON DELETE CASCADE
+FOREIGN KEY (idPost) REFERENCES Posts(idPost) ON DELETE CASCADE
+);
+
+
+/*Key has to exist in both tables*/
+CREATE TABLE Visitors(
+idCountry int primary key not null,
+idPost int not null,
+nameCountry varchar(50),
+FOREIGN KEY (idPost) REFERENCES Posts(idPost) ON DELETE CASCADE
 );
 
 
 
-/*4 - */
+
+/*4 - Zdvojení klíče pozor*/
+
+INSERT INTO Visitors(idCountry, idPost, nameCountry)
+VALUES (7, 1,  "CZE"),
+ (8, 2,  "LEB"),
+ (9, 3,  "USA"),
+ (10, 4,  "FRA")
 
 INSERT INTO Posts(idPost, published, heading, content)
-VALUES (1, "2018-08-09", "Hey there", "Lore ipsum...");
-VALUES (2, "2019-03-09", "Hey there", "Lore ipsum...");
-VALUES (3, "2019-06-09", "Hey there", "Lore ipsum...");
-VALUES (4, "2019-08-03", "Hey there", "Lore ipsum...");
+VALUES (7, '2018-08-09', "Hey there", "Lore ipsum..."),
+ (8, '2019-03-09', "Hey there", "Lore ipsum..."),
+ (9, '2019-06-09', "Hey there", "Lore ipsum..."),
+ (10, '2019-08-03', "Hey there", "Lore ipsum...")
 
 
-INSERT INTO PostSettings(idSettings, private);
-VALUES(1, 0);
-VALUES(2, 0);
-VALUES(3, 1);
-VALUES(4, 1);
+INSERT INTO PostSettings(idSettings, idPost, private)
+VALUES
+(1, 1, 0),
+(2, 2, 0),
+(3, 3, 1),
+(4, 4, 1)
 
 
-INSERT INTO Comments(idComment, content);
-VALUES(1, "Hello");
-VALUES(2, "Mello");
-VALUES(3, "Mello");
-VALUES(4, "Mello");
-
+INSERT INTO Comments(idComment, idPost, content)
+VALUES(1, 1, "Comment 1"),
+(2,2, "Comment 2"),
+(3, 3, "Comment 3"),
+(4, 4, "Comment 4");
 
 /*5 Queries*/
+
+/*SELECT POST WITH ID 1*/
+SELECT content FROM Posts WHERE idPost = 1;
+
+
+
+
+
+
+
+/* FIX BELOW*/
 
 /*POST LONGER THAN 100 CHARACTERS*/
 SELECT content FROM Posts WHERE LEN(content) > 100;
@@ -58,9 +87,9 @@ SELECT idPost FROM Posts LEFT JOIN PostSettings ON Posts.idPosts = PostSettings.
 /*SELECT ALL POSTS FROM 2018*/
 SELECT content FROM Posts WHERE DATEPART(YEAR(date)) = 2018;
 
-/*SELECT POST WITH ID 1*/
-SELECT content FROM Posts WHERE id = 1;
+
 
 /*GROUP POSTS BY YEAR */
 
 SELECT content FROM Posts WHERE id = 1;
+
