e148e69391f2dacdee809cb1e57c4cf1ca12e3d4
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-12-15 19:36:40 +0100

    Update Project.sql

--- DBS-Exam-1/Project.sql
+++ DBS-Exam-1/Project.sql
@@ -26,14 +26,14 @@ content varchar(50) not null,
 FOREIGN KEY (idPost) REFERENCES Posts(idPost) ON DELETE CASCADE,
 FOREIGN KEY (idPublisher) REFERENCES Publishers(idPublisher) ON DELETE CASCADE
 ); 
---- Works
 
 
--- ORDER
+
 CREATE TABLE PostCountries(
 idCountry int primary key not null,
 nameCountry varchar(50)
 );
+
 
 
 /*Key has to exist in both tables*/
@@ -65,21 +65,22 @@ VALUES (1, 1, '2018-08-09', "Hey there", "Lore ipsum...", 0),
  (3, 3, '2019-06-09', "Hey there", "Lore ipsum...", 1),
  (4, 4, '2019-08-03', "Hey there", "Lore ipsum...", 1)
  
- 
+INSERT INTO Posts(idPost, idPublisher, published, heading, content, private)
+VALUES (8, 1, '2018-08-07', "Hey there", "Another comment", 0)
 /* COUNTRIES */
-
-INSERT INTO PostCountries(idCountry, nameCountry)
-VALUES (1, "CZE"),
- (2, "LEB"),
- (3, "USA"),
- (4, "MAR")
 
 
 INSERT INTO Visitors(idVisitors, idCountry, idPost, visitDate)
-VALUES (7, 1, 1, '2018-08-09'),
+-- VALUES (7, 10, 1, '2018-08-09'),
+VALUES (17, 10, 1, '2018-08-09'),
  (8, 2,  2, '2018-08-09'),
  (9, 3,  3, '2019-08-09'),
  (10, 4, 4, '2019-08-09')
+ 
+ INSERT INTO Visitors(idVisitors, idCountry, idPost, visitDate)
+VALUES (17, 10, 1, '2018-08-09')
+
+
 
 INSERT INTO Comments(idComment, idPost, idPublisher, content)
 VALUES(1, 1, 1, "Comment 1"),
@@ -88,100 +89,77 @@ VALUES(1, 1, 1, "Comment 1"),
 (4, 4, 4, "Comment 4")
 
 
+INSERT INTO PostCountries(idCountry, nameCountry)
+VALUES (9, "CZE"), (10, "USA"), (11, "GBR")
 
 
+/*
+INSERT INTO PostCountries(idCountry, idPost, nameCountry)
+VALUES (1, 1, "CZE"),
+ (2, 2, "LEB"),
+ (3, 3, "USA"),
+ (4, 4, "MAR")*/
 
 /* 15 Queries*/
 
-/*1*/
+
+/*1 M*/
+SELECT Posts.idPost FROM Posts 
+LEFT JOIN Visitors ON Posts.idPost = Visitors.idPost
+LEFT JOIN PostCountries ON PostCountries.idCountry = Visitors.idCountry
+WHERE nameCountry = "CZE"; -- 1
+
+
+/* GET VISIT DATE OF USA VISTOR
+2019-08-09 */
+SELECT visitDate FROM Visitors 
+LEFT JOIN PostCountries ON Visitors.idCountry = PostCountries.idCountry 
+WHERE PostCountries.idCountry = 10;
+
+
+/* 2 M */
+SELECT Posts.content FROM Posts 
+LEFT JOIN Comments ON Posts.idPost = Comments.idPost 
+WHERE Comments.idPost = 1;
+-- ORDER BY LENGTH(Comments.content) ASC;
+
+
+
+
+
+/* 3 M Show Posts Published by "Filip" */
+SELECT Posts.content FROM Posts LEFT JOIN Publishers ON Posts.idPublisher = Publishers.idPublisher WHERE Publishers.namePublisher = "Filip Vabroušek";
+
+/* 5 - Get count of posts for each Publisher */
+SELECT Count(*), Publishers.namePublisher FROM Posts LEFT JOIN Publishers ON Posts.idPublisher = Publishers.idPublisher GROUP BY Publishers.namePublisher; 
+
+/*6*/
 SELECT content FROM Posts WHERE idPost = 1;
 
+/*7*/
 SELECT COUNT(*) FROM Posts;
 
-/*KOMENT85E Kpostu s ID*/
+/*8 agregace*/
+SELECT MAX(LENGTH(content)) FROM Posts;
 
+/*9*/
+SELECT AVG(LENGTH(content)) FROM Posts;
 
+/*10*/
+UPDATE Posts SET content = "Hey" WHERE idPost = 3;
 
-
-
-
-/*2
-SELECT Posts.idPost FROM Posts LEFT JOIN PostSettings ON Posts.idPost = PostSettings.idSettings WHERE private = 1;*/
-/* 3 SELECT ALL POSTS WITH CZECH VISITORS */
-SELECT Posts.idPost FROM Posts 
-LEFT JOIN Visitors ON Posts.idPost = Visitors.idPost 
-LEFT JOIN Posts.idPost = Visitors.idPost WHERE nameCountry = "CZE";
-
-/* 4 COMMENTS SORTED BY LENGTH */
-SELECT Posts.content FROM Posts LEFT JOIN Comments ON Posts.idPost = Comments.idComment ORDER BY LENGTH(Comments.content) ASC;
-/*SELECT 3 LONGEST PUBLIC POSTS*/
-SELECT Posts.content FROM Posts LEFT JOIN PostSettings ON Posts.idPost = PostSettings.idSettings WHERE private = 0 GROUP BY LENGTH(content) LIMIT 3;
-
-
-/*AVG POST LENGTH FRO LEBANON*/
-SELECT AVG(LENGTH(Posts.content)) FROM Posts LEFT JOIN Visitors ON Posts.idPost = Visitors.idPost WHERE Visitors.idCountry = 8;
-
-/* 4 SELECT LONGEST STRING */
-SELECT content FROM Posts ORDER BY LENGTH(content) DESC LIMIT 1;
-/* 5 */
-UPDATE Posts SET heading = "Je to tady!" WHERE idPost = 1;
-/* 6 */
-DELETE FROM Posts WHERE idPost = 1;
-/* 7 */
-SELECT AVG(LENGTH(content)) FROM Posts; 
-/* 8 */
-SELECT content FROM Posts WHERE LEN(content) > 10;
-/* 9 */
-
-/* 13 */
-UPDATE Visitors SET idPost = 21 WHERE nameCountry = "CZE";
-
-/* 14 */
-INSERT INTO Posts(idPost, published, heading, content)
-VALUES (120, '2018-08-09', "Hey there", "Lore ipsum...");
-
-/* 15 */
+/*11*/
 DELETE FROM Comments WHERE content LIKE "%1%";
 
+/*12 - vložení nového postu*/
+INSERT INTO Posts(idPost, idPublisher, published, heading, content, private)
+VALUES (9, 1, '2018-08-07', "Hey there", "Another comment", 0)
 
-/*SELECT POST WITH COMMENT 1 TEXT
-SELECT Comments.content FROM Comments LEFT JOIN Posts ON Comments.idComment = Posts.idPost WHERE Commments.content = "Comment 1";
-*/
+/*13 - funkce - -extrakce prvních 3. písmen */
+SELECT SUBSTRING(heading, 1, 3) FROM Posts WHERE Posts.idPost = 2; -- hey
 
+/*14*/
+SELECT UPPER(content) FROM Comments GROUP BY idPublisher;
 
-
-/*
-Vytvořte minimálně 15 zcela odlišných dotazů (minimálně 5 dotazů musí vybírat data z více jak jedné tabulky): 
-výběrové s kritériem/podmínkou, 
-výběrové s použitím agregací, 
-výběrové s použitím funkce, 
-akční dotaz – přidávací, 
-akční dotaz – odstraňovací, 
-akční dotaz – aktualizační. 
-*/
-
-
-
-
-
-
-
-
-
-
-/*
-SELECT CR 
-FROM table1 
-WHERE len(CR) = (SELECT max(len(CR)) FROM table1)
-*/
-
-
-/*POST LONGER THAN 100 CHARACTERS*/
-SELECT content FROM Posts WHERE LEN(content) > 100;
-
-/* FIX BELOW*/
-/*SELECT ALL POSTS FROM 2018*/
-SELECT content FROM Posts WHERE published = 2018;
-SELECT content FROM Posts WHERE DATEPART("year", published) = 2018;
- /*SELECT DATEDIFF("yyyy", published, getDate()) FROM Posts WHERE id = 1;*/
- SELECT DATEDIFF(published, getDate()) FROM Posts WHERE idPost= 1;
+/*15*/
+SELECT COUNT(*) FROM Comments WHERE idPublisher = 2; 
