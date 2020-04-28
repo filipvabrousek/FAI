014c1ca970a4dc5cbeba076c07c38b97b709e3cf
Filip Vabrousek <filipvabrousek1@gmail.com>
2020-04-28 20:21:01 +0200

    Create IPHONE-ADD.swift

---
+++ IPHONE-ADD.swift
@@ -0,0 +1,7 @@
+  // ADITION SEPTEMBER 2019
+            case "iPhone12,1":                              return "iPhone 11"
+            case "iPhone12,3":                              return "iPhone 11 Pro"
+            case "iPhone12,5":                              return "iPhone 11 Pro Max"
+            // ADDITION APRIL 2020
+            case "iPhone12,8":                              return "iPhone SE (2nd generation)"
+            // OTHER
