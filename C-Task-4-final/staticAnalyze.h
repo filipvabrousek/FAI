bcfe15aa6a7f0a0c536f2c8e8531a558ca7476ed
Filip Vabrousek <filipvabrousek1@gmail.com>
2020-05-10 18:20:16 +0200

    Create staticAnalyze.h

---
+++ C-Task-4-final/staticAnalyze.h
@@ -0,0 +1,14 @@
+//
+//  staticAnalyze.h
+//  C-Task-4-correction
+//
+//  Created by Filip Vabroušek on 10/05/2020.
+//  Copyright © 2020 Filip Vabroušek. All rights reserved.
+//
+
+#ifndef staticAnalyze_h
+#define staticAnalyze_h
+
+#include <stdio.h>
+
+#endif /* staticAnalyze_h */
